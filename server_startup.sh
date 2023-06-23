8dd923fb3f7015c21610d245b6f1196123770d7a
ppsdatta <sdatta@planview.com>
2023-06-23 17:29:29 +0530

    Add daemon script

--- server_startup.sh
+++ server_startup.sh
@@ -2,3 +2,7 @@
 apt update
 apt install -y wget
 wget "https://raw.githubusercontent.com/souravdatta/msngr/main/server.pl"
+wget "https://raw.githubusercontent.com/souravdatta/msngr/main/demon.pl"
+perl demon.pl
+echo "++++++startup done++++++"
+
